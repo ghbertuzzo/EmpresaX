86e2fcf5873c5f008de19c7d7fe54088de59a272
giovani bertuzzo <giovani.hbertuzzo@gmail.com>
2024-05-30 13:12:06 -0300

    add script create database, tables and index
    add script create database, tables and index

---
+++ script-create.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE empresa_x;
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255),
+    telefone VARCHAR(20),
+    email VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255),
+    preco DECIMAL(10, 2) NOT NULL,
+    estoque INT NOT NULL
+);
+
+CREATE TABLE vendas (
+    id SERIAL PRIMARY KEY,
+
+    ,
+    cliente_id INT NOT NULL,
+    data_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (cliente_id) REFERENCES clientes (id)
+);
+
+CREATE TABLE itens_venda (
+    id SERIAL PRIMARY KEY,
+    venda_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_unitario DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (venda_id) REFERENCES vendas (id),
+    FOREIGN KEY (produto_id) REFERENCES produtos (id)
+);
+
+CREATE INDEX idx_cliente_email ON clientes (email);
+CREATE INDEX idx_produto_nome ON produtos (nome);
+CREATE INDEX idx_venda_cliente ON vendas (cliente_id);
+CREATE INDEX idx_itens_venda_venda ON itens_venda (venda_id);
+CREATE INDEX idx_itens_venda_produto ON itens_venda (produto_id);
